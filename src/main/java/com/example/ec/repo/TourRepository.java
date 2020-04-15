9470d6dec0c4ab3d3b66be03da95d88f44583276
Ankit Raj Prasad <prasad.ankitarj@gmail.com>
2020-04-15 13:35:36 +0530

    More

--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -8,6 +8,9 @@ inside the package - com.example.ec.repo
 import com.example.ec.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(String code);
 
 }
